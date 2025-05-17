e21c3e7de6a3be7fea25b0ad408b2daf6293354f
Kunal Hulke <hulke1102002@gmail.com>
2025-05-18 00:31:51 +0530

    adding userSchema

---
+++ server/db/models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+    trim: true,
+    min: 3,
+    max: 30,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+    min: 10,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+userSchema.pre("save", async function (next) {
+  const user = this;
+  if (!user.isModified("password")) {
+    next();
+  }
+  user.password = await bcrypt.hash(user.password, 10);
+});
+
+userSchema.methods.generateJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.SECRET_KEY, {
+    expiresIn: "5d",
+  });
+};
+module.exports = mongoose.model("users", userSchema);
