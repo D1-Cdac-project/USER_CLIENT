f93b8ff281336d26496d1b1162cf1cd4cbb295f8
Kunal Hulke <hulke1102002@gmail.com>
2025-05-18 00:32:52 +0530

    adding changes into server.js and also add extra dependencies

--- server/server.js
+++ server/server.js
@@ -1,11 +1,32 @@
-const express=require('express');
-const app=express();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const env = require("dotenv");
+const userRouter = require("./routes/user");
 
+const app = express();
 
-app.get('/',(req,res)=>{
-    res.send("this is get request")
-})
+env.config();
+mongoose.set("strictQuery", true);
 
-app.listen('localhost',4000,()=>{
-    console.log("Server is running on port 4000")
-})
+app.use(
+  cors({
+    origin: ["http://localhost:5500", "http://localhost:3000"],
+    methods: ["GET", "PUT", "POST", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization", "x-csrf-token"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+
+// MongoDB connections
+mongoose.connect(process.env.MONGODB_CONNECTION).then(() => {
+  console.log("Database connected");
+});
+
+//middlewares
+app.use("/api/user", userRouter);
+
+app.listen(process.env.PORT, () => {
+  console.log("Server is running on port " + process.env.PORT);
+});
