18e42a10f381890e600812242598617683ccbfc2
krh1102002 <hulke1102002@gmail.com>
2025-08-07 12:48:54 +0530

    added user setup and also integrated tailwind css into it

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to My Application</h1>
+      <p>This is a simple React application.</p>
+    </div>
+  );
+}
+
+export default App;
