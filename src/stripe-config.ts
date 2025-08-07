a57b3b4349bf80e3a886ff00e120a13a0c11bc4d
krh1102002 <hulke1102002@gmail.com>
2025-08-07 12:49:36 +0530

    Revert "added user setup and also integrated tailwind css into it"
    This reverts commit 18e42a10f381890e600812242598617683ccbfc2.

--- src/stripe-config.ts
+++
@@ -1,9 +0,0 @@
-export const products = {
-  'Vidya Vikas Photography': {
-    priceId: 'price_1RPoM3SFr2YZLF7tmJ2JMKO6',
-    description: 'sjdlfjas',
-    mode: 'subscription'
-  }
-} as const;
-
-export type ProductId = keyof typeof products;
